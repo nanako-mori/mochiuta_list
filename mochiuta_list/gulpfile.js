3c6c813a724839890b39a67f6813788c81c1022b
Nanako Mori <nanako.mori19871229@gmail.com>
2021-08-10 14:36:10 +0900

    gulp-plumber, gulp-notifyをインストール

--- mochiuta_list/gulpfile.js
+++ mochiuta_list/gulpfile.js
@@ -1,12 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+var plumber = require("gulp-plumber");
+var notify = require("gulp-notify");
 
-gulp.task('sass', function() {
-  return gulp.src('./static/sass/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('./static/css'));
+gulp.task("sass", function () {
+    return gulp
+        .src("./static/sass/*.scss")
+        .pipe(
+            plumber({
+                errorHandler: notify.onError("Error: <%= error.message %>"),
+            })
+        )
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(gulp.dest("./static/css"));
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch('./static/sass/*.scss', gulp.task('sass'));
+gulp.task("sass:watch", function () {
+    gulp.watch("./static/sass/*.scss", gulp.task("sass"));
 });
