76d04f6627f84f7b1abc820eb477640f366afa18
Nanako Mori <nanako.mori19871229@gmail.com>
2021-08-11 10:51:14 +0900

    globをインストール

--- mochiuta_list/gulpfile.js
+++ mochiuta_list/gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass")(require("sass"));
+var sassGlob = require("gulp-sass-glob");
 var plumber = require("gulp-plumber");
 var notify = require("gulp-notify");
 
 gulp.task("sass", function () {
     return gulp
         .src("./static/sass/*.scss")
+        .pipe(sassGlob())
         .pipe(
             plumber({
                 errorHandler: notify.onError("Error: <%= error.message %>"),
